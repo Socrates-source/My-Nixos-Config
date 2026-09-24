d6572a464eb0f26ff837cbb116710485b5aebf14
Socrates-source <bharadwajshaurya2020@gmail.com>
2026-09-24 23:27:05 +0530

    Helium

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    helium.url = "github:tomsch/helium-nix";
   };
 
-  outputs = { self, nixpkgs, ... }
+  outputs = { self, nixpkgs, helium, ... }
  @inputs: {
        nixosConfigurations = {
             Travelmate = 
@@ -13,6 +14,10 @@
                system = "x86_64-linux";
                modules = [
                   ./configuration.nix
+               { environment.systemPackages = [
+                  helium.packages.x86_64-linux.default
+           ];
+          }   
         ];
       };
     };      
